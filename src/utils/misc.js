19bda9ed9276c3d9627082e21ab1b1c3a53a92c7
rahad123 <rahaddiu@gmail.com>
2022-10-25 00:05:49 +0600

    refactor: run lintfix

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,16 +1,16 @@
 const misc = {
-    keySort: (obj) => {
-        const keys = Object.keys(obj).sort();
-        let sortedObj = {};
-        for(let i in keys) {
-            sortedObj[keys[i]] = obj[keys[i]];
-        }
-
-        return sortedObj;
-    },
-    toJsObject: async (input) => {
-        return JSON.parse(JSON.stringify(input));
+  keySort: (obj) => {
+    const keys = Object.keys(obj).sort();
+    let sortedObj = {};
+    for (let i in keys) {
+      sortedObj[keys[i]] = obj[keys[i]];
     }
+
+    return sortedObj;
+  },
+  toJsObject: async (input) => {
+    return JSON.parse(JSON.stringify(input));
+  },
 };
 
 export { misc };
