082ca3dc17ca75d69ea5b020ab04c4b109f38277
rahad123 <rahaddiu@gmail.com>
2022-10-25 00:04:11 +0600

    refactor: run lintfix

--- test/user.test.js
+++ test/user.test.js
@@ -1,45 +1,44 @@
-import mongoose from 'mongoose';
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import { assert } from 'chai';
-import faker from 'faker';
-import { User } from '../src/users/user.model.js';
+import mongoose from "mongoose";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import { assert } from "chai";
+import faker from "faker";
+import { User } from "../src/users/user.model.js";
 // import { sample } from 'lodash';
-import { service } from '../src/users/user.service.js';
+import { service } from "../src/users/user.service.js";
 import slugify from "slugify";
 import { _p } from "../src/helpers/asyncWrapper.js";
-import { server } from ".././server.js"
+import { server } from ".././server.js";
 
 chai.use(chaiHttp);
 
-const createUser = async(args = null) => {
+const createUser = async (args = null) => {
+  const userInput = {
+    id: mongoose.Types.ObjectId,
+    username: slugify(faker.lorem.words(2)),
+    email: faker.internet.email(),
+    password: faker.lorem.words(2),
+  };
+  const input = args ? args : userInput;
+  const user = await service.createUser(input);
+  return user;
+};
+
+describe("User test suit", () => {
+  it("should create a new user", async () => {
+    const userId = mongoose.Types.ObjectId();
+
     const userInput = {
-        id: mongoose.Types.ObjectId,
-        username: slugify(faker.lorem.words(2)),
-        email: faker.internet.email(),
-        password: faker.lorem.words(2),
-    }
-    const input = args ? args : userInput;
-    const user = await service.createUser(input);
-    return user;
-}
-
-describe('User test suit', () => {
-
-    it('should create a new user', async () => {
-        const userId = mongoose.Types.ObjectId();
-
-        const userInput = {
-            username: slugify(faker.lorem.words(2)),
-            email: faker.internet.email(),
-            password: faker.lorem.words(2),
-        };
-        const[err, res] = await _p(
-            chai
-                .request(server)
-                .post('/graphql')
-                .send({
-                   query: `
+      username: slugify(faker.lorem.words(2)),
+      email: faker.internet.email(),
+      password: faker.lorem.words(2),
+    };
+    const [err, res] = await _p(
+      chai
+        .request(server)
+        .post("/graphql")
+        .send({
+          query: `
                    mutation {
                     createUser(
                         createUserInput : {
@@ -54,15 +53,14 @@ describe('User test suit', () => {
                            password     
                         }
                 }
-                   `, 
-                })
-        );
+                   `,
+        })
+    );
 
-        if(res.body.errors) console.error(res.body.errors);
-        const user = res.body.data.createUser;
-        console.log(user);
+    if (res.body.errors) console.error(res.body.errors);
+    const user = res.body.data.createUser;
+    console.log(user);
 
-        assert.isDefined(user, 'user should be defined');
-    })
+    assert.isDefined(user, "user should be defined");
+  });
 });
-
